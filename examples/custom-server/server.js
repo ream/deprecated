b920b7b01a6c8bbd94a4e7b81e07668c757d142d
EGOIST <0x142857@gmail.com>
2017-03-21 01:00:30 +0800

    fix typo

--- examples/custom-server/server.js
+++ examples/custom-server/server.js
@@ -5,7 +5,7 @@ unvue(app, {
   // do not build before starting production server
   // instead run `npm run build` before `npm start` yourself
   // because if you're deploying it to service like `now.sh`
-  // you're only allowed to write files in `npm run build` script
+  // you're only allowed to write files outside `npm run build` script
   build: false,
   postCompile() {
     console.log(`> Open http://localhost:3000`)
