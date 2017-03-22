a4e1ed9e6dd0611f75a312489396fdeaddcd1a1b
EGOIST <0x142857@gmail.com>
2017-03-23 00:49:04 +0800

    rename context name on client-side

--- app/client-entry.js
+++ app/client-entry.js
@@ -7,8 +7,8 @@ const app = createApp(entry)
 
 // prime the store with server-initialized state.
 // the state is determined during SSR and inlined in the page markup.
-if (window.__INITIAL_STATE__ && store) {
-  store.replaceState(window.__INITIAL_STATE__)
+if (window.__UNVUE__.state && store) {
+  store.replaceState(window.__UNVUE__.state)
 }
 
 // wait until router has resolved all async before hooks
