f652fc876134be191151419fe4473d6487e21f35
EGOIST <0x142857@gmail.com>
2017-03-25 14:04:44 +0800

    Simplify core

--- app/server-entry.js
+++ app/server-entry.js
@@ -1,6 +1,5 @@
 import entry from '@alias/entry'
 import createApp from './create-app'
-import { setAsyncData } from './mixins/async-data'
 
 const { router, store } = entry
 const app = createApp(entry)
@@ -11,11 +10,6 @@ const meta = app.$meta()
 
 export default context => {
   const s = isDev && Date.now()
-  const cache = context.cache
-
-  if (!process.__CACHE) {
-    process.__CACHE__ = cache
-  }
 
   return new Promise((resolve, reject) => {
     router.push(context.url)
@@ -29,49 +23,17 @@ export default context => {
       }
 
       Promise.all(matchedComponents.map(component => {
-        let pipe = Promise.resolve()
-        const { preFetch, preFetchCache, asyncData, name } = component
+        const { preFetch } = component
 
         if (preFetch) {
-           pipe = pipe.then(() => preFetch({ store, route }))
-        } else {
-          if (preFetchCache && name) {
-            const key = `preFetchCache:${name}:${route.fullPath}`
-            const cacheData = cache && cache.get(key)
-            pipe = pipe.then(() => {
-              return preFetchCache({ store, cache: cacheData, route }).then(newCacheData => {
-                if (newCacheData) {
-                  context.preFetchCacheKeys.push(key)
-                  cache && cache.set(key, newCacheData)
-                }
-              })
-            })
-          }
+           return preFetch({ store, route })
         }
-
-        if (asyncData && name) {
-          pipe = pipe.then(() => {
-            const data = asyncData({ store, route })
-            const key = `asyncData:${name}:` + route.fullPath
-            if (data.then) {
-              return data.then(newCacheData => {
-                if (newCacheData) {
-                  context.asyncDataKeys.push(key)
-                  cache && cache.set(key, newCacheData)
-                }
-              })
-            }
-          })
-        }
-
-        return pipe
       })).then(() => {
         isDev && console.log(`> Data pre-fetch: ${Date.now() - s}ms`)
         if (store) {
           context.state = store.state
         }
         context.meta = meta
-
         resolve(app)
       }).catch(reject)
     })
--- examples/pre-fetch-cache/README.md
+++
@@ -1,1 +0,0 @@
-The first visit should be about 2000ms, after that it will be less than 100ms.
