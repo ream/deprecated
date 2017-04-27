ba631596a53662d388ed099b73e669fbb974a8f3
EGOIST <0x142857@gmail.com>
2017-04-27 23:27:17 +0800

    clean up

--- app/server-entry.js
+++ app/server-entry.js
@@ -1,23 +1,9 @@
 import Vue from 'vue'
 import createApp from './create-app'
 
-let hasGlobalMixin
-
 export default ssrContext => {
   const dev = ssrContext.dev
   const s = dev && Date.now()
-
-  if (!hasGlobalMixin) {
-    hasGlobalMixin = true
-    Vue.mixin({
-      created() {
-        for (const key in ssrContext.data) {
-          console.log
-          this[key] = ssrContext.data[key]
-        }
-      }
-    })
-  }
 
   return new Promise((resolve, reject) => {
     const { app, router, store } = createApp(ssrContext)
@@ -34,18 +20,6 @@ export default ssrContext => {
         if (Component.preFetch) {
           ps.push(Component.preFetch(ctx))
         }
-        // if (Component.asyncData) {
-        //   ps.push(Component.asyncData(ctx).then(data => {
-        //     ssrContext.data.asyncData = ssrContext.data.asyncData || []
-        //     ssrContext.data.asyncData[index] = data
-        //     const dataFn = Component.data
-        //     Component.data = function () {
-        //       const defaultData = dataFn ? dataFn.call(this) : {}
-        //       return {...defaultData, ...data}
-        //     }
-        //     console.log(Component)
-        //   }))
-        // }
         return Promise.all(ps)
       })).then(() => {
         ssrContext.state = store.state
