16abb357f00e3c98704a770f959b541eec46839c
EGOIST <0x142857@gmail.com>
2017-06-01 15:24:56 +0800

    rename ssrContext to context

--- app/server-entry.js
+++ app/server-entry.js
@@ -1,29 +1,29 @@
 import Vue from 'vue'
 import createApp from './create-app'
 
-export default ssrContext => {
-  const dev = ssrContext.dev
+export default context => {
+  const dev = context.dev
   const s = dev && Date.now()
 
   return new Promise((resolve, reject) => {
-    const { app, router, store } = createApp(ssrContext)
+    const { app, router, store } = createApp(context)
 
-    router.push(ssrContext.url)
+    router.push(context.url)
 
     router.onReady(() => {
       const route = router.currentRoute
       const matchedComponents = router.getMatchedComponents()
       Promise.all(matchedComponents.map((Component, index) => {
         if (Component.preFetch) {
-          const ctx = { route, store, req: ssrContext.req }
+          const ctx = { route, store, req: context.req }
           return Component.preFetch(ctx)
         }
       })).then(() => {
         if (store) {
-          ssrContext.state = store.state
+          context.state = store.state
         }
         if (app.$meta) {
-          ssrContext.meta = app.$meta()
+          context.meta = app.$meta()
         }
         resolve(app)
       }).catch(reject)
