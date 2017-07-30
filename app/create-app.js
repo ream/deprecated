23c911845f240f6a3d96de95112eada43ddc84de
EGOIST <0x142857@gmail.com>
2017-07-30 20:53:49 +0800

    add option to disable runInNewContext

--- app/create-app.js
+++ app/create-app.js
@@ -29,8 +29,8 @@ export default context => {
     )
   }
 
-  const router = entry.createRouter(context)
-  const store = entry.createStore && entry.createStore(context)
+  const router = entry.router ? entry.router : entry.createRouter(context)
+  const store = entry.store ? entry.store : (entry.createStore && entry.createStore(context))
 
   if (store && router) {
     sync(store, router)
