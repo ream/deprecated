ad6c38086ec8e7f0c63148708ee1c4bd071bd483
Ilya Semenov <semenov@inetss.com>
2018-06-05 01:41:57 +0700

    Fix typo in before-client-render event name

--- app/client-entry.js
+++ app/client-entry.js
@@ -106,7 +106,7 @@ Vue.mixin({
 // Wait until router has resolved all async before hooks
 // and async components...
 async function main() {
-  event.$emit('before-client-renderer')
+  event.$emit('before-client-render')
 
   if (window.__REAM__.initialData) {
     dataStore.replaceState(window.__REAM__.initialData)
