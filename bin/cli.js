6810a8fb0e8d11a8df1d57bf8039610e50c07037
EGOIST <0x142857@gmail.com>
2018-08-15 21:01:39 +0800

    only select require options from cli flags

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const cac = require('cac')
 const chalk = require('chalk')
+const merge = require('lodash.merge')
 const logger = require('../lib/logger')
 
 const major = parseInt(process.versions.node.split('.')[0], 10)
@@ -31,24 +32,32 @@ const logUrl = app => {
   })
 }
 
-const getOptions = (dev, input, flags) => {
+const getOptions = (command, input, flags) => {
+  const dev = command === 'dev'
   const options = {
-    ...flags,
     dev,
     baseDir: input[0]
+  }
+  if (command === 'dev' || command === 'start') {
+    if (flags.host) {
+      options.server = merge(options.server, { host: flags.host })
+    }
+    if (flags.port) {
+      options.server = merge(options.server, { port: flags.port })
+    }
   }
   return options
 }
 
 cli.command('build', 'Build your application', (input, flags) => {
-  const options = getOptions(false, input, flags)
+  const options = getOptions('build', input, flags)
   const app = require('../lib')(options)
   return app.build()
 })
 
 cli
   .command('dev', 'Develop your application', (input, flags) => {
-    const options = getOptions(true, input, flags)
+    const options = getOptions('dev', input, flags)
     const app = require('../lib')(options)
     logUrl(app)
     return app.start()
@@ -58,7 +67,7 @@ cli
 
 cli
   .command('start', 'Start your application in production', (input, flags) => {
-    const options = getOptions(false, input, flags)
+    const options = getOptions('start', input, flags)
     const app = require('../lib')(options)
     logUrl(app)
     return app.start()
@@ -67,7 +76,7 @@ cli
   .option('port', 'Server port (default: 4000)')
 
 cli.command('generate', 'Generate static html files', (input, flags) => {
-  const options = getOptions(false, input, flags)
+  const options = getOptions('generate', input, flags)
   const app = require('../lib')(options)
   return app.generate()
 })
