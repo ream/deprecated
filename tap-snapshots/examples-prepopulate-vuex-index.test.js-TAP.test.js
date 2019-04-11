a42ca0e8e641ac6153b7d883576520c91263f671
Ahmedov Samir <samgold05@gmail.com>
2019-04-11 16:36:06 +0300

    add html-minifier

--- tap-snapshots/examples-prepopulate-vuex-index.test.js-TAP.test.js
+++ tap-snapshots/examples-prepopulate-vuex-index.test.js-TAP.test.js
@@ -8,21 +8,5 @@
 exports[
   `examples/prepopulate-vuex/index.test.js TAP examples/prepopulate-vuex > page 1`
 ] = `
-<!DOCTYPE html><html>
-  <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui" />
-    
-      <title data-ream-head="true"></title>
-      
-      
-      
-      <link rel="preload" href="/_ream/chunk-vendors.js" as="script"><link rel="preload" href="/_ream/client.js" as="script">
-  </head>
-  <body>
-    <div id="_ream" data-server-rendered="true"><h1>hello1</h1></div>
-    <script>window.__REAM__={"initialData":{},"state":{"count":1}}</script><script src="/_ream/chunk-vendors.js" defer></script><script src="/_ream/client.js" defer></script>
-  </body>
-</html>
+<!doctype html><html><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no,minimal-ui"><title data-ream-head="true"></title><link rel="preload" href="/_ream/chunk-vendors.js" as="script"><link rel="preload" href="/_ream/client.js" as="script"></head><body><div id="_ream" data-server-rendered="true"><h1>hello1</h1></div><script>window.__REAM__={"initialData":{},"state":{"count":1}}</script><script src="/_ream/chunk-vendors.js" defer="defer"></script><script src="/_ream/client.js" defer="defer"></script></body></html>
 `
--- tap-snapshots/test-projects-errors-index.test.js-TAP.test.js
+++ tap-snapshots/test-projects-errors-index.test.js-TAP.test.js
@@ -8,21 +8,5 @@
 exports[
   `test/projects/errors/index.test.js TAP test/projects/errors server-side 404 > undefined 1`
 ] = `
-<!DOCTYPE html><html>
-  <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui" />
-    
-      <title data-ream-head="true"></title>
-      
-      
-      
-      <link rel="preload" href="/_ream/chunk-vendors.js" as="script"><link rel="preload" href="/_ream/client.js" as="script"><link rel="prefetch" href="/_ream/chunk--base-pages-error.js"><link rel="prefetch" href="/_ream/chunk--base-pages-index.js">
-  </head>
-  <body>
-    <div id="_ream" data-server-rendered="true"><div><h1>404: page not found</h1></div></div>
-    <script>window.__REAM__={"initialData":{}}</script><script src="/_ream/chunk-vendors.js" defer></script><script src="/_ream/client.js" defer></script>
-  </body>
-</html>
+<!doctype html><html><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no,minimal-ui"><title data-ream-head="true"></title><link rel="preload" href="/_ream/chunk-vendors.js" as="script"><link rel="preload" href="/_ream/client.js" as="script"><link rel="prefetch" href="/_ream/chunk--base-pages-error.js"><link rel="prefetch" href="/_ream/chunk--base-pages-index.js"></head><body><div id="_ream" data-server-rendered="true"><div><h1>404: page not found</h1></div></div><script>window.__REAM__={"initialData":{}}</script><script src="/_ream/chunk-vendors.js" defer="defer"></script><script src="/_ream/client.js" defer="defer"></script></body></html>
 `
